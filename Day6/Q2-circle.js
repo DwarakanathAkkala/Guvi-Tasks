d7289adc0f7b49ec6567e170d7f15db741a9e547
DwarakanathAkkala <dwarakanath.akkala@gmail.com>
2022-12-07 13:53:06 +0530

    Q2, Q3 commit

--- Day6/Q2-circle.js
+++ Day6/Q2-circle.js
@@ -1,6 +1,45 @@
-class Circle {
-    constructor(radius, color) {
-        this.radius = radius;
-        this.color = color;
+let circle = {
+    raduis: "1",
+    color: "Red",
+    set setRadius(newRadius){
+        this.radius = newRadius;
+    },
+    set setColor(newColor) {
+        this.color = newColor;
+    },
+    get getRadius(){
+        console.log(`Radius of Circle is ${this.radius}`);
+    },
+    get getColor(){
+        console.log(`Color of Circle is ${this.color}`);
+    },
+    get toString(){
+        console.log(`Circle[radius = ${this.radius}, color = ${this.color}]`);
+    },
+    get getArea(){
+        let area = Math.PI * this.radius * this.radius;
+        console.log(`Area of Circle is ${area}`);
+    },
+    get getCircumference(){
+        let circumference = 2 * Math.PI * this.radius;
+        console.log(`Circumference of Circle is ${circumference}`);
     }
 }
+
+// Setter Methods
+circle.setRadius = 9;
+circle.setColor = "Blue";
+
+// Getter Methods
+circle.getRadius;
+circle.getColor;
+circle.toString;
+circle.getArea;
+circle.getCircumference;
+
+// Output
+// Radius of Circle is 9
+// Color of Circle is Blue
+// Circle[radius = 9, color = Blue]
+// Area of Circle is 254.46900494077323
+// Circumference of Circle is 56.548667764616276
--- Day6/Q3-person.js
+++ Day6/Q3-person.js
@@ -1,12 +1,31 @@
 class Person {
-    constructor(firstName, lastName, age, email) {
+    constructor(firstName, lastName, age, email, distance, eyeColor) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
         this.email = email;
+        this.distance = distance;
+        this.eyeColor = eyeColor;
     }
 
     introduceSelf() {
-        console.log(`My name is ${this.firstName} ${this.lastName} and my age is ${age}`);
+        console.log(`Hello, my name is ${this.firstName} ${this.lastName} and my age is ${this.age}`);
+    }
+
+    walk() {
+        console.log(`I walk about ${this.distance}km as a daily routine.`);
     }
 }
+
+// Instantiation
+let person1 = new Person("Dwarakanath", "Akkala", 25, "dwaruu3.0@gmail.com", 3, "brown");
+
+person1.introduceSelf();
+
+// Output
+// My name is Dwarakanath Akkala and my age is 25
+
+person1.walk();
+
+// Output
+// I walk about 3km as a daily routine.
