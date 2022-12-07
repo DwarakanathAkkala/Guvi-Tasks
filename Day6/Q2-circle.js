449549406e8ea230e2cd8e2c7c37b29799055f17
DwarakanathAkkala <dwarakanath.akkala@gmail.com>
2022-12-07 14:08:27 +0530

    updated class

--- Day6/Q2-circle.js
+++ Day6/Q2-circle.js
@@ -1,45 +1,50 @@
-let circle = {
-    raduis: "1",
-    color: "Red",
-    set setRadius(newRadius){
+class Circle{
+
+    // Setter Methods
+    setRadius(newRadius){
         this.radius = newRadius;
-    },
-    set setColor(newColor) {
+    }
+    setColor(newColor) {
         this.color = newColor;
-    },
-    get getRadius(){
+    }
+
+    // Getter Methods
+    getRadius(){
         console.log(`Radius of Circle is ${this.radius}`);
-    },
-    get getColor(){
+    }
+    
+    getColor(){
         console.log(`Color of Circle is ${this.color}`);
-    },
-    get toString(){
+    }
+    
+    toString(){
         console.log(`Circle[radius = ${this.radius}, color = ${this.color}]`);
-    },
-    get getArea(){
+    }
+    
+    getArea(){
         let area = Math.PI * this.radius * this.radius;
         console.log(`Area of Circle is ${area}`);
-    },
-    get getCircumference(){
+    }
+    
+    getCircumference(){
         let circumference = 2 * Math.PI * this.radius;
         console.log(`Circumference of Circle is ${circumference}`);
     }
 }
 
-// Setter Methods
-circle.setRadius = 9;
-circle.setColor = "Blue";
+let circle = new Circle();
+circle.setRadius(1);
+circle.setColor("Red");
 
-// Getter Methods
-circle.getRadius;
-circle.getColor;
-circle.toString;
-circle.getArea;
-circle.getCircumference;
+circle.getRadius();
+circle.getColor(); 
+circle.toString();
+circle.getArea();
+circle.getCircumference();
 
 // Output
-// Radius of Circle is 9
-// Color of Circle is Blue
-// Circle[radius = 9, color = Blue]
-// Area of Circle is 254.46900494077323
-// Circumference of Circle is 56.548667764616276
+// Radius of Circle is 1
+// Color of Circle is Red
+// Circle[radius = 1, color = Red]
+// Area of Circle is 3.141592653589793
+// Circumference of Circle is 6.283185307179586
